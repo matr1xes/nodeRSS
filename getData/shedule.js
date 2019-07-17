28d192236e9eb2c3f8b14145e650199f35095508
Matrixes <a358482873@outlook.com>
2019-07-17 20:08:03 +0800

    change fetch time.

--- getData/shedule.js
+++ getData/shedule.js
@@ -1,13 +1,12 @@
-const schedule = require('node-schedule')
-const moment = require('moment')
-const start = require('./cnbeta_task')
+const schedule = require("node-schedule");
+const moment = require("moment");
+const start = require("./cnbeta_task");
 
 const timer = () => {
-    schedule.scheduleJob('50 */2 * * * *', function (fireDate) {
-        console.log(`\nTime:${moment(fireDate).format('YY-MM-DD HH:mm:ss')}`)
-        start()
-    })
+  schedule.scheduleJob("50 */2 * * * *", function(fireDate) {
+    console.log(`\nTime:${moment(fireDate).format("YY-MM-DD HH:mm:ss")}`);
+    start();
+  });
+};
 
-}
-
-module.exports = timer
+module.exports = timer;
